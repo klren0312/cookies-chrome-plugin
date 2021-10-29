abae3ff6d428e50a77a056f9222e9ade932922d6
klkl <kuanglong0312@sina.com>
2021-10-29 14:00:17 +0800

    feat: 插件popup框显示cookies和ua

--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,27 @@
-getTabId()
+document.addEventListener('DOMContentLoaded', ()=> {
+  getTabId()
+})
 
 function getTabId () {
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    document.getElementById('current-main').innerText = tabs.length ? tabs[0].id: '暂无'
+    const currentTab = tabs.length ? tabs[0] : null
+    if (currentTab) {
+      document.getElementById('current-main').innerText = currentTab ? currentTab.id: '暂无'
+      const url = new URL(currentTab.url)
+      
+      chrome.cookies.getAll({
+        domain: url.hostname
+      }, cookies => {
+        const list = document.getElementById('js-cookies-list')
+        const listStr = cookies.map(c => {
+          return `<tr>
+              <td><strong>${c.name}: </strong></td>
+              <td><input style="width:100%" value=${c.value} /></td>
+          </tr>`
+        }).join('')
+        list.innerHTML = listStr
+      })
+      document.getElementById('js-user-agent').innerHTML = navigator.userAgent
+    }
   })
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  var cleanBtn = document.querySelector('.clean-btn')
-  cleanBtn.addEventListener('click', function() {
-    document.getElementById('current-main').innerText = ''
-    getTabId()
-  })
-})
